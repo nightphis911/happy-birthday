2fe0205220fa8101707ba51bfe01a3fc7b59d907
nightphis911 <egaega665@gmail.com>
2024-10-04 20:58:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function showGift() {
     document.getElementById("giftImage").style.display = "block";
     document.getElementById("message").style.display = "block";
     
-    // Mengubah warna latar belakang ke warna pilihan
-    document.body.style.backgroundColor = "pink"; // Ganti dengan warna favorit Anda
+    // Mengubah warna latar belakang
+    document.body.style.backgroundColor = "pink"; // Ganti dengan warna pilihan Anda
+    
+    // Memutar lagu
+    const audio = document.getElementById("audio");
+    audio.play();
 }
