85735750c1fb531d62a18fba1fbf069acb9c931a
nightphis911 <egaega665@gmail.com>
2024-10-04 20:55:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function showGift() {
     document.getElementById("giftImage").style.display = "block";
     document.getElementById("message").style.display = "block";
+    
+    // Mengubah warna latar belakang ke warna pilihan
+    document.body.style.backgroundColor = "pink"; // Ganti dengan warna favorit Anda
 }
