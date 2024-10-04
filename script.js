3c9f137777ef9e2789b9dade3c1ffaf62edac6c9
nightphis911 <egaega665@gmail.com>
2024-10-04 20:45:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function showGift() {
-    const message = "Semoga hari ini dipenuhi dengan kebahagiaan dan cinta!";
-    document.getElementById('message').innerText = message;
-    
-    const giftReveal = document.getElementById('gift-reveal');
-    const presentImage = document.querySelector('.present');
-    
-    giftReveal.classList.remove('hidden');
-    presentImage.style.display = 'block';
+    document.getElementById("giftImage").style.display = "block";
+    document.getElementById("message").style.display = "block";
 }
