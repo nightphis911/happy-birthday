b392f8134cbfc7a93fb21c6b93ac98b21bcd25ea
nightphis911 <egaega665@gmail.com>
2024-10-04 21:18:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,5 @@ function showGift() {
     // Menampilkan pesan unduhan setelah jeda 5 detik
     setTimeout(() => {
         document.getElementById("downloadMessage").style.display = "block";
-    }, 2000); // 5000 milidetik = 5 detik
+    }, 1000); // 5000 milidetik = 5 detik
         }
